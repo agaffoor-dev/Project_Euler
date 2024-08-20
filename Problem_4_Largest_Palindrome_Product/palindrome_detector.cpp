9d0b2f3dc3519c94b67d5a96947a19e49e48cb2f
agaffoor-dev <174390644+agaffoor-dev@users.noreply.github.com>
2024-08-20 11:36:06 -0400

    Added pseudocode for palindrome checker

---
+++ Problem_4_Largest_Palindrome_Product/palindrome_detector.cpp
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+/*
+Ask user to enter number
+  Use mechanism to split number into separate digits
+  Perform palindrome check with adjusted conditions for even/odd number of indexes
+    n[0] == n[size - 1], n[1] == n[size - 2], etc...
+  If valid, cout it
+  If not, continue
+*/
